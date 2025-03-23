5d0d0035029a29fbafd6c830cfae0cb9034c8b8e
Annamalai <bharanimurugan1@gmail.com>
2025-03-23 19:33:21 +0530

    modified file xyz print message

--- src/Helloworld/xyz.java
+++ src/Helloworld/xyz.java
@@ -3,7 +3,8 @@ package Helloworld;
 public class xyz {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Modified the files");
+		System.out.println("second line");
 
 	}
 
